00a8696d85f57f0f789112be53e0c9e6a5c283c9
lv-hoang <luongvanhoang500@gmail.com>
2026-09-22 06:24:31 +0700

    fix: aggressively block all video itags using regexFilter to prevent any video data leakage

--- background.js
+++ background.js
@@ -21,8 +21,8 @@ function updateAudioOnlyRule(enabled) {
         priority: 1,
         action: { type: 'block' },
         condition: {
-          urlFilter: 'mime=video',
-          domains: ['googlevideo.com']
+          regexFilter: "^https?://[^/]+\\.(googlevideo\\.com|c\\.youtube\\.com)/videoplayback.*[?&](mime=video|itag=(133|134|135|136|137|138|160|212|242|243|244|247|248|264|266|271|272|278|298|299|302|303|308|313|315|330|331|332|333|334|335|336|337|394|395|396|397|398|399))(&|$)",
+          resourceTypes: ["xmlhttprequest", "media"]
         }
       }],
       removeRuleIds: [AUDIO_ONLY_RULE_ID]
